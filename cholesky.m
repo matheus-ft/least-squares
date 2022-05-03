71a8260eb459ba67575110d2f83e2a7bd938b8fe
matheus <71662383+matheus-ft@users.noreply.github.com>
2022-05-03 18:36:46 -0300

    Update function

--- cholesky.m
+++ cholesky.m
@@ -1,23 +1,24 @@
 function R = cholesky(A)
-	n = rows(A);  % safe, since we are assuming that A is symmetric, thus it is a square matrix
+% A is a symmetric, thus square, matrix
+
+	n = rows(A);
 	R = zeros(n);
 
 	for i = 1 : n
 		for j = 1 : n
+
 			% fill the diagonal of the Cholesky factor
 			if (i == j)
-				% sum of the square of the elements below the diagonal
 				sum = 0;
 				for k = 1 : i-1
 					sum += R(k, i) ^ 2;
 				endfor
 				% checks if the matrix A is positive definite
 				if (sum >= A(i, i))
-					printf("%s\n", "A is not positive definite");
-					return;
+					error("A is not positive definite");
 				endif
 				R(i, i) = sqrt( A(i, i) - sum );
-			
+
 			% fill upper half of the Cholesky factor
 			elseif (j > i)
 				sum = 0;
@@ -25,11 +26,12 @@ function R = cholesky(A)
 					sum += R(k, i) * R(k, j);
 				endfor
 				R(i, j) = ( A(i, j) - sum ) / R(i, i);
-			
+
 			% bellow diagonal
 			else
 				R(i, j) = 0;
 			endif
+
 		endfor
 	endfor
 
