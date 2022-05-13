0494dde3ead37a686fffb2e3cfde5960a1a266a5
matheus <71662383+matheus-ft@users.noreply.github.com>
2022-05-12 22:19:05 -0300

    Separate backward substitution function from symmetric linear system solver

--- linear_sys.m
+++ linear_sys.m
@@ -5,18 +5,6 @@ function x = linear_sys(A, b)
     x = forward_subs(R', y);
 endfunction
 
-function x = backward_subs(A, b)
-    n = rows(A);
-    x = zeros(n, 1);
-    for i = n : -1 : 1  % for i in range(n, 0, -1)
-        summation = 0;
-        for j = n : -1 : i+1
-            summation += x(j) * A(i, j)
-        endfor
-        x(i) = (b(i) - summation) / A(i, i)
-    endfor
-endfunction
-
 function x = forward_subs(A, b)
     n = rows(A);
     x = zeros(n, 1);
