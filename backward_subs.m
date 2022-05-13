4effa2ed84470cee36a14f2a46408794732d081a
matheus <71662383+matheus-ft@users.noreply.github.com>
2022-05-13 08:51:45 -0300

    Fix formatting

--- backward_subs.m
+++ backward_subs.m
@@ -1,11 +1,11 @@
 function x = backward_subs(A, b)
     n = rows(A);
     x = zeros(n, 1);
-    for i = n : -1 : 1  % for i in range(n, 0, -1)
+    for i = n : -1 : 1  # for i in range(n, 0, -1)
         summation = 0;
         for j = n : -1 : i+1
-            summation += x(j) * A(i, j)
+            summation += x(j) * A(i, j);
         endfor
-        x(i) = (b(i) - summation) / A(i, i)
+        x(i) = (b(i) - summation) / A(i, i);
     endfor
 endfunction
