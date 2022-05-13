0de066b4a177b205f056854637bdcf9bdc698c63
matheus <71662383+matheus-ft@users.noreply.github.com>
2022-05-13 16:44:31 -0300

    Create project entry point to solve the least squares problems

--- grab_data.m
+++ grab_data.m
@@ -1,9 +1,5 @@
 function [A, b] = grab_data(filename)
-    b = dlmread(filename, "")(:,2);
-    interval = 1:rows(b);
-    interval = interval - (rows(b) / 2);  % this way we reduce the absolute value of the elements in A
-    A = [];
-    for i = 1:rows(b)
-        A(i, :) = interval(i) .^ [0:10];
-    endfor
+    b = dlmread(filename, "")(:,2);  % data vector
+    interval = [1:rows(b)]';  % column vector as index for the data vector
+    A = interval .^ [0:10];
 endfunction
