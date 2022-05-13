8ad6dd462b6986a1e0143696c9baa9d97443c4c6
matheus <71662383+matheus-ft@users.noreply.github.com>
2022-05-13 08:51:45 -0300

    Fix formatting

--- linear_sys.m
+++ linear_sys.m
@@ -1,6 +1,8 @@
 function x = linear_sys(A, b)
+    # A is symmetric, thus, we try to find its Cholesky factor R
+    # If A = RR', since Ax = b -> RR'x = b
+    # Therefore, we want x: Ry = b and R'x = y
     R = cholesky(A);
-    % A = RR' and Ax = b <-> RR'x = b <-> Ry = b and R'x = y
     y = backward_subs(R, b);
     x = forward_subs(R', y);
 endfunction
@@ -11,8 +13,8 @@ function x = forward_subs(A, b)
     for i = 1 : n
         summation = 0;
         for j = 1 : i-1
-            summation += x(j) * A(i, j)
+            summation += x(j) * A(i, j);
         endfor
-        x(i) = (b(i) - summation) / A(i, i)
+        x(i) = (b(i) - summation) / A(i, i);
     endfor
 endfunction
