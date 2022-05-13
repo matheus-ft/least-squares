7cc1f94bc75449a958c2ef9f3ab190d394707286
matheus <71662383+matheus-ft@users.noreply.github.com>
2022-05-13 17:12:47 -0300

    Update readme

--- backward_subs.m
+++ backward_subs.m
@@ -1,7 +1,7 @@
 function x = backward_subs(A, b)
     n = rows(A);
     x = zeros(n, 1);
-    for i = n : -1 : 1  # for i in range(n, 0, -1)
+    for i = n : -1 : 1  % python = for i in range(n, 0, -1): ...
         summation = 0;
         for j = n : -1 : i+1
             summation += x(j) * A(i, j);
